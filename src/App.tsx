3597bca2bc61f439541d33512a0ee0da3e824317
Joshua <tank158r5155@yahoo.com.tw>
2021-06-06 08:17:17 +0800

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { books } from './books';
+import logo from './logo.svg';
+import { Book } from './types';
+
+const LIMIT = 5;
+const START_POINT = {
+  BOOK: '彌迦書',
+  CHAPTER: 7,
+};
+
+const times = (num: number) => Array(num).fill(0);
+
+const concatBooks = (books: Book[]) => {
+  return books.reduce((acc: Book[], cur: Book) => {
+    return [
+      ...acc,
+      ...times(cur.chapters).map((_, i) => ({ name: cur.name, chapters: ++i })),
+    ];
+  }, []);
+};
+
+const getStartCharter = (allBooks: Book[]) => {
+  return allBooks.findIndex(
+    (book: Book) =>
+      book.name === START_POINT.BOOK && book.chapters === START_POINT.CHAPTER
+  );
+};
+
+const formatOutput = (plans) => {
+  return plans.map((plan) => {
+    var str = Object.keys(plan)
+      .map((key) => `${key}: ${plan[key]}`)
+      .join(', ');
+    console.log(str);
+  });
+};
 
 function App() {
+  const allBooks = concatBooks(books);
+  const start = getStartCharter(allBooks);
+
+  const plans = times(6).map((_, i) => {
+    const next = allBooks.slice(start + i++ * LIMIT, start + i++ * LIMIT);
+    return next.reduce((acc: { [name: string]: string }, cur) => {
+      if (!acc[cur.name]) {
+        return {
+          ...acc,
+          [cur.name]: cur.chapters.toString() + '章',
+        };
+      }
+
+      const hasDash = acc[cur.name].includes('-');
+      const separator = hasDash ? '-' : '章';
+
+      return {
+        ...acc,
+        [cur.name]: `${acc[cur.name].split(separator)[0]}-${cur.chapters}章`,
+      };
+    }, {});
+  });
+
+  plans.forEach((plan) => {
+    var str = Object.keys(plan)
+      .map((key) => `${key}: ${plan[key]}`)
+      .join(', ');
+    console.log(str);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export interface Book {
+  chapters: number;
+  name: string;
+}
